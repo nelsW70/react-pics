30ba4e15b838a71ee7c6fcb792b00aae74c7340c
Nelson Foltz <nelsfoltz@gmail.com>
2020-07-12 16:54:12 -0400

    add: bind onFormSubmit method in constructor

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,8 +3,15 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
+  constructor(props) {
+    super(props);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
   onFormSubmit(event) {
     event.preventDefault();
+
+    console.log(this.state.term);
   }
 
   render() {
