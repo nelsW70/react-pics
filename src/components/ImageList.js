a5f8b4a9a09c18b5c3722a3b59dbbb525bbe5e2c
Nelson Foltz <nelsfoltz@gmail.com>
2020-07-13 10:48:48 -0400

    add: key to each image

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const ImageList = (props) => {
-  const images = props.images.map((image) => {
-    return <img src={image.urls.regular} alt={image.urls.alt_description} />;
-  });
+  const images = props.images.map((image) => (
+    <img
+      key={image.id}
+      src={image.urls.regular}
+      alt={image.urls.alt_description}
+    />
+  ));
 
   return <div>{images}</div>;
 };
